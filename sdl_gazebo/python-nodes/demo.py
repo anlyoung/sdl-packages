8ab16b03062f7d58dd17a5e04b1d346257c473fa
Lawrence <lawrenceztang@gmail.com>
2021-07-10 12:10:06 -0400

    Working wheel controllers with python

--- sdl_gazebo/python-nodes/demo.py
+++ sdl_gazebo/python-nodes/demo.py
@@ -1,16 +1,33 @@
-from rospy import Subscriber
+from rospy import Subscriber, Publisher
 from sensor_msgs.msg import PointCloud2
 from time import sleep
 import rospy
+from geometry_msgs.msg import Twist
 
+class MirRobot:
 
-def main():
-    rospy.init_node("python_sub")
-    depth_sub = Subscriber("/camera/depth/points", PointCloud2, depth_callback)
-    rospy.spin()
+    def run(self):
+        rospy.init_node("python_sub")
 
-def depth_callback(params):
-    print(params)
+        self.depth_sub = Subscriber("/camera/depth/points", PointCloud2, self.depth_callback)
+
+        self.wheel_pub = Publisher("/wheel_controller/cmd_vel", Twist, queue_size=1)
+        while not rospy.is_shutdown():
+            self.move_forward()
+
+    def depth_callback(self, params):
+        print(params)
+
+    def navigate(self):
+        return
+
+    def move_forward(self):
+        msg = Twist()
+        msg.linear.x = 10
+        msg.linear.y = 10
+        msg.linear.z = 10
+        self.wheel_pub.publish(msg)
 
 if __name__ == "__main__":
-    main()
+    robot = MirRobot()
+    robot.run()
