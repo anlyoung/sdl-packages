8517743d8bfb3851eacb1cc3fba90335a30b505d
Lawrence <lawrenceztang@gmail.com>
2021-07-12 12:39:18 -0400

    Navigation working

--- sdl_gazebo/python-nodes/demo.py
+++ sdl_gazebo/python-nodes/demo.py
@@ -1,31 +1,57 @@
+from __future__ import print_function
 from rospy import Subscriber, Publisher
-from sensor_msgs.msg import PointCloud2
+from sensor_msgs.msg import Image
 from time import sleep
 import rospy
 from geometry_msgs.msg import Twist
+import numpy as np
+import math
+
 
 class MirRobot:
 
     def run(self):
         rospy.init_node("python_sub")
 
-        self.depth_sub = Subscriber("/camera/depth/points", PointCloud2, self.depth_callback)
+        self.action = "forward"
+
+        self.depth_sub = Subscriber("/camera/depth/image_raw", Image, self.depth_callback)
 
         self.wheel_pub = Publisher("/wheel_controller/cmd_vel", Twist, queue_size=1)
-        while not rospy.is_shutdown():
-            self.move_forward()
+
+        self.navigate()
 
     def depth_callback(self, params):
-        print(params)
+
+        from cv_bridge import CvBridge
+        bridge = CvBridge()
+        image = bridge.imgmsg_to_cv2(params, "32FC1")
+
+        for i in range(len(image)):
+            for j in range(len(image[i])):
+                if not math.isnan(image[i][j]) and i == 200:
+                    print("turn")
+                    self.action = "turn"
+                    return
+        self.action = "forward"
+
+    def turn(self):
+        msg = Twist()
+        msg.angular.x = .5
+        msg.angular.y = .5
+        msg.angular.z = .5
+        self.wheel_pub.publish(msg)
 
     def navigate(self):
-        return
+        while not rospy.is_shutdown():
+            if self.action == "forward":
+                self.move_forward()
+            elif self.action == "turn":
+                self.turn()
 
     def move_forward(self):
         msg = Twist()
         msg.linear.x = 10
-        msg.linear.y = 10
-        msg.linear.z = 10
         self.wheel_pub.publish(msg)
 
 if __name__ == "__main__":
